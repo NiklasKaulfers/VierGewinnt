56a9e04c2ce2afe70f69c728d8873721dec15197
Niklas Kaulfers <niklaskaulfers2@gmail.com>
2025-12-08 00:06:40 +0100

    Unit tests for IsTopOfColumn

--- src/test/logic/board/IsTopOfColumnTest.java
+++ src/test/logic/board/IsTopOfColumnTest.java
@@ -11,13 +11,55 @@ public class IsTopOfColumnTest {
 
     BoardTestInterface board;
     @BeforeEach
+    @DisplayName("GIVEN board with dimensions 6x6")
     void setUp() {
         board = new Board();
     }
 
     @Test
-    @DisplayName("WHEN column 1 had 6 stones placed THEN return 0")
+    @DisplayName("WHEN column 1 has 3 stones placed THEN return 3")
+    void halfwayFullColumnTest(){
+        board.placeStone(1);
+        board.placeStone(1);
+        board.placeStone(1);
+
+
+        Assertions.assertEquals(3, board.isTopOfColumn(1));
+    }
+
+    @Test
+    @DisplayName("WHEN column 1 has 6 stones THEN return 0")
     void fullColumnTest(){
+        board.placeStone(1);
+        board.placeStone(1);
+        board.placeStone(1);
+        board.placeStone(1);
+        board.placeStone(1);
+        board.placeStone(1);
+
+        Assertions.assertEquals(0, board.isTopOfColumn(1));
+    }
+
+    @Test
+    @DisplayName("WHEN column 1 has 0 stones THEN return 6")
+    void emptyColumnTest(){
+        Assertions.assertEquals(6, board.isTopOfColumn(1));
+    }
+
+    @Test
+    @DisplayName("GIVEN a 10x10 board WHEN column 10 has 0 stones THEN return 10")
+    void emptyColumn10Test(){
+        board = new Board(10, 10);
+        Assertions.assertEquals(10, board.isTopOfColumn(9));
+    }
+
+    @Test
+    @DisplayName("GIVEN a 10x10 board WHEN column 1 has 10 stones THEN return 0")
+    void fullColumn10Test(){
+        board = new Board(10, 10);
+        board.placeStone(1);
+        board.placeStone(1);
+        board.placeStone(1);
         board.placeStone(1);
         board.placeStone(1);
         board.placeStone(1);
@@ -29,4 +71,15 @@ public class IsTopOfColumnTest {
         Assertions.assertEquals(0, board.isTopOfColumn(1));
     }
 
+    @Test
+    @DisplayName("WHEN column -1 is called THEN do nothing")
+    void nothingTest(){
+        Assertions.assertEquals(0, board.isTopOfColumn(-1));
+    }
+
+    @Test
+    @DisplayName("WHEN column 7 is called THEn do nothing")
+    void nothing7Test(){
+        Assertions.assertEquals(0, board.isTopOfColumn(7));
+    }
 }
